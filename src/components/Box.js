738f72c215f6b1cc7675aa34d787196f0ea66258
Matthew Creek <matthew.creek27@gmail.com>
2023-04-27 15:46:27 -0500

    independent box state

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 export default function Box(props) {
+
+    const [boxColor, setBoxColor] = React.useState(props.square.on)
+
     const styles = {
-        backgroundColor: props.square.on ? '#222222' : '#cccccc',
-      }
+        backgroundColor: boxColor ? '#222222' : '#cccccc',
+    }
+
+    function handleClick() {
+        setBoxColor(prevBoxColor => !prevBoxColor)
+    }
 
     return (
-        <div style={styles} className="box"></div>
+        <div style={styles} className="box" onClick={handleClick}></div>
     )
 }
