537ccb17b92e42267735ce684387bb75f382cb8f
Matthew Creek <matthew.creek27@gmail.com>
2023-04-27 15:22:48 -0500

    box challenge part 1 complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Header from './components/Header';
-import Body from './components/Body';
+import boxes from "./data/boxes";
 
-export default function App() {
-  const [user, setUser] = React.useState("Joe")
+export default function App(props) {
+  /**
+   * Challenge part 1:
+   * 1. Initialize state with the default value of the
+   *    array pulled in from boxes.js
+   * 2. Map over that state array and display each one
+   *    as an empty square (black border, transparent bg color)
+   *    (Don't worry about using the "on" property yet)
+   */
+
+  const [squares, setSquares] = React.useState(boxes)
+  const styles = {
+    backgroundColor: props.darkMode ? '#222222' : '#cccccc',
+  }
+
+  const squareElements = squares.map(square => (
+    <div style={styles} className="box" key={square.id}></div>
+  ))
 
   return (
-      <main>
-          <Header user={user} />
-          <Body user={user} />
-      </main>
-  )
+    <main>
+      {squareElements}
+    </main>
+  );
 }
