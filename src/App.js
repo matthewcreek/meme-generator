664a0bf1ddf129ae8c574260c52af8bbf28715db
Matthew Creek <matthew.creek27@gmail.com>
2023-04-27 13:34:18 -0500

    initial counter state commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './App.css';
 
 import Header from "./components/Header";
 import Meme from './components/Meme';
+import Count from './components/Count';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
+
   return (
     <div>
-      <Header />
-      <Meme />
+      <div className="counter">
+        <button className="counter--minus" onClick={subtract}>–</button>
+        <Count number={count} />
+        <button className="counter--plus" onClick={add}>+</button>
+      </div>
+      {/* <Header /> */}
+      {/* <Meme /> */}
     </div>
   );
 }
