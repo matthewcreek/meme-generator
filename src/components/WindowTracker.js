8f2cb6279d7a17254a6a11de4d940dc385a0b367
Matthew Creek <matthew.creek27@gmail.com>
2023-07-17 22:19:18 -0500

    window width updating via event listener and state

--- src/components/WindowTracker.js
+++ src/components/WindowTracker.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function WindowTracker() {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener("resize", function() {
+            setWindowWidth(window.innerWidth)
+        })
+    }, [])
+
     return (
-        <h1>Window width: {window.innerWidth}</h1>
+        <h1>Window width: {windowWidth}</h1>
     )
 }
