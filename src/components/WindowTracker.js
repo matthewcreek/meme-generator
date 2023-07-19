c1cd05a7474ba700a7d6981beed7635cd3e91684
Matthew Creek <matthew.creek27@gmail.com>
2023-07-18 21:33:40 -0500

    added cleanup function for useEffect

--- src/components/WindowTracker.js
+++ src/components/WindowTracker.js
@@ -5,9 +5,15 @@ export default function WindowTracker() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
-        window.addEventListener("resize", function() {
+        function watchWidth() {
             setWindowWidth(window.innerWidth)
-        })
+        }
+
+        window.addEventListener("resize", watchWidth)
+        
+        return function() {
+            window.removeEventListener("resize", watchWidth)
+        }
     }, [])
 
     return (
